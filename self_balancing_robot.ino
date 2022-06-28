fc96c178d8f89c1e7a99b7d3b7df85cd590387c3
kaburia <kaburiaaustin1@gmail.com>
2022-06-28 05:22:13 +0300

    first changes

--- self_balancing_robot.ino
+++ self_balancing_robot.ino
@@ -1,12 +1,28 @@
+
+// TB6612FNG Pins
+// right
+#define PWMA 9
 #define IN1M 7
 #define IN2M 6
+// left
 #define IN3M 11
 #define IN4M 12
-#define PWMA 9
 #define PWMB 10
+
 #define STBY 8
 
+// Encoder
+#define encode_left 2
+#define encode_right 4
+
+#define ppr 360
+
 int pwm = 255;
+volatile long encodervalue = 0;
+float rpm;
+int interval = 1000;
+long previousMillis = 0;
+long currentMillis = 0;
 
 void setup (){
     Serial.begin(9600);
@@ -17,25 +33,57 @@ void setup (){
     pinMode(IN3M, OUTPUT);
     pinMode(IN4M, OUTPUT);
     pinMode(STBY, OUTPUT);
+    pinMode(encode_left, INPUT_PULLUP);
+    pinMode(encode_right, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(encode_left), UpdateEncoder, RISING);
+    attachInterrupt(digitalPinToInterrupt(encode_right), UpdateEncoder, RISING);
+    previousMillis = millis();
 
 }
 
 
 void loop(){
-    analogWrite(PWMA, pwm);
-    analogWrite(PWMB, pwm);
+  // Both motors moving forward
+    analogWrite(PWMA, 220);
+    analogWrite(PWMB, 200);
     digitalWrite(IN1M, HIGH);
     digitalWrite(IN2M, LOW);
     digitalWrite(IN3M, LOW);
     digitalWrite(IN4M, HIGH);
     digitalWrite(STBY, HIGH);
     delay(1000);
-//    digitalWrite(IN3M, LOW);
-//    digitalWrite(IN4M, HIGH);
-//    digitalWrite(IN3M, LOW);
-//    digitalWrite(IN1M, LOW);
-//    digitalWrite(IN2M, HIGH);
-//    delay(1000);
-    
+    rpm = (float)((encodervalue * 60) / ppr)/3;
+
+   //to update milliseconds every second
+  currentMillis = millis();
+  if (currentMillis - previousMillis > interval){
+    previousMillis = currentMillis;
+  }
+
+  if(pwm > 0 || rpm > 0){
+    Serial.print("PWM: ");
+    Serial.print(pwm);
+    Serial.print('\t');
+    Serial.print("PULSE: ");
+    Serial.print(encodervalue);
+    Serial.print('\t');
+    Serial.print("SPEED: ");
+    Serial.print(rpm);
+    Serial.println(" RPM");
+  }
+
+
+
+}
+
+
+void UpdateEncoder(){
+    if (digitalRead(encode_left) == digitalRead(encode_right)){
+      encodervalue ++;
+    }
+    else;
+     {
+      encodervalue --;
+     }
 
 }
